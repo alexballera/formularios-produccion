2240d67b38935b2aae4a2b9ecc70402811eb612b
Alex Ballera <alexballera@gmail.com>
2016-12-16 09:37:11 -0400

    probando con db

--- mail.php
+++ mail.php
@@ -35,17 +35,34 @@ $header .= 'From:' . $email. '\r\n'; // Sender's Email
 // send email
 $success = mail($emailTo, $subject, $body, $header);
 
-$connection = mysql_connect("localhost", "root", ""); // Establishing Connection with Server..
-$db = mysql_select_db("aballera_formularios", $connection); // Selecting Database
-if (isset($name)) {
-$query = mysql_query("insert into form_element(name, lastname, phone, email, message) values ('$name', '$phone', '$email','$message')"); //Insert Query
-echo "Form Submitted succesfully";
-}
-mysql_close($connection); // Connection Closed
+$enlace = mysqli_connect("127.0.0.1", "aballera_alex", "Juan03:16", "aballera_formularios");
+
+// $connection = mysql_connect("localhost", "root", ""); // Establishing Connection with Server..
+// $db = mysql_select_db("aballera_formularios", $connection); // Selecting Database
+// if (isset($name)) {
+// $query = mysql_query("insert into form_element(name, lastname, phone, email, message) values ('$name', '$phone', '$email','$message')"); //Insert Query
+// echo "Form Submitted succesfully";
+// }
+// mysql_close($connection); // Connection Closed
 
 // redirect to success page
 if ($success){
   echo '<script language="javascript">alert("Tu consulta ha sido enviada correctamente.");</script>';
+  if (!$enlace) {
+      echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
+      echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
+      echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
+      exit;
+  } else {
+    if (isset($name)) {
+    $query = mysqli_query("insert into aballera_formularios(name, lastname, phone, email, message) values ('$name', '$phone', '$email','$message')"); //Insert Query
+    echo "Form Submitted succesfully";
+    }
+    echo "Éxito: Se realizó una conexión apropiada a MySQL! La base de datos mi_bd es genial." . PHP_EOL;
+    echo "Información del host: " . mysqli_get_host_info($enlace) . PHP_EOL;
+
+    mysqli_close($enlace);
+  }
 }else{
   echo '<script language="javascript">alert("Revisa los datos ingresados");</script>';
 }
